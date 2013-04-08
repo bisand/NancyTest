34b0298bec859396658f10ac72dfdb837d50b266
André Biseth <andre.biseth@pareto.no>
2013-04-08 15:20:56 +0200

    Initial commit

---
+++ NancyTest/Modules/HelloModule.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Divan;
+using Nancy;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NancyTest.Modules
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+            Get["/cars/{hp?}"] = parameters =>
+                {
+                    var server = new CouchServer();
+                    var db = server.GetDatabase("trivial");
+                    var tempView = db.NewTempView("test", "test", "if (doc.docType && doc.docType == 'car') emit(doc.Hps, doc);");
+                    var linqCars = tempView.LinqQuery<Car>();
+                    if (parameters.hp)
+                    {
+                        var hp = (int) parameters.hp;
+                        var fastCars = linqCars.Where(c => c.HorsePowers == hp).ToList();
+                        return Response.AsJson(fastCars);
+                    }
+                    var cars = linqCars.Where(x=>x.HorsePowers <= 500).ToList();
+                    return Response.AsJson(cars);
+                };
+            Get["/init/{count?100}"] = parameters =>
+                {
+                    var server = new CouchServer();
+                    var db = server.GetDatabase("trivial");
+                    for (var i = 0; i < parameters.count; i++)
+                    {
+                        var car = new Car("Saab", "93", 170 + i);
+                        db.SaveDocument(car);
+                    }
+                    return string.Format("Generated {0} cars.", parameters.count);
+                };
+        }
+    }
+
+    public class Car : CouchDocument
+    {
+        public string Make;
+        public string Model;
+        public int HorsePowers;
+
+        public Car()
+        {
+            // This constructor is needed by Divan
+        }
+
+        public Car(string make, string model, int hps)
+        {
+            Make = make;
+            Model = model;
+            HorsePowers = hps;
+        }
+        #region CouchDocument Members
+
+        public override void WriteJson(JsonWriter writer)
+        {
+            // This will write id and rev
+            base.WriteJson(writer);
+
+            writer.WritePropertyName("docType");
+            writer.WriteValue("car");
+            writer.WritePropertyName("Make");
+            writer.WriteValue(Make);
+            writer.WritePropertyName("Model");
+            writer.WriteValue(Model);
+            writer.WritePropertyName("Hps");
+            writer.WriteValue(HorsePowers);
+        }
+
+        public override void ReadJson(JObject obj)
+        {
+            // This will read id and rev
+            base.ReadJson(obj);
+
+            Make = obj["Make"].Value<string>();
+            Model = obj["Model"].Value<string>();
+            HorsePowers = obj["Hps"].Value<int>();
+        }
+
+        #endregion
+    }
+
+}
---
+++ NancyTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Nancy.Hosting.Self;
+using RedBranch.Hammock;
+
+namespace NancyTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var uri = "http://localhost:8888";
+            Console.WriteLine(uri);
+            // initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
+            var host = new NancyHost(new Uri(uri));
+            host.Start();  // start hosting
+
+            //Under mono if you deamonize a process a Console.ReadLine with cause an EOF 
+            //so we need to block another way
+            if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                while (true)
+                    Thread.Sleep(10000000);
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+
+            host.Stop();  // stop hosting
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public IList<string> Categories { get; set; }
+    }
+
+    internal class MyObject
+    {
+        public string Name { get; set; }
+    }
+}
