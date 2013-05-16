be073d1b46dc52016415534f729a148e44a4841c
André Biseth <andre@biseth.net>
2013-05-17 01:12:29 +0200

    Getting the session to work

--- NancyTest/Modules/HelloModule.cs
+++ NancyTest/Modules/HelloModule.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-using Divan;
+using Biseth.Net.Settee;
 using Nancy;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -13,36 +13,39 @@ namespace NancyTest.Modules
         {
             Get["/"] = parameters =>
                 {
-                    var indexModel = new IndexModel();
-                    indexModel.Username = "Unknown";
-                    indexModel.CurrentDate = DateTime.Now;
+                    var indexModel = new IndexModel
+                        {
+                            Username = "Unknown", 
+                            CurrentDate = DateTime.Now
+                        };
                     return View["Views/index.html", indexModel];
                 };
             Get["/cars/{hp?}"] = parameters =>
                 {
-                    var server = new CouchServer();
-                    var db = server.GetDatabase("trivial");
-                    var tempView = db.NewTempView("test", "test",
-                                                  "if (doc.docType && doc.docType == 'car') emit(doc.Hps, doc);");
-                    var view = db.Query("car", "ByHps");
-                    var linqCars = tempView.LinqQuery<Car>();
-                    if (parameters.hp)
+                    var database = new CouchDatabase("http://localhost:5984/");
+                    using (var session = database.OpenSession("trivial"))
                     {
-                        var hp = (int) parameters.hp;
-                        var car = linqCars.Where(c => c.HorsePowers == hp).ToList();
-                        return Response.AsJson(car);
+                        if (parameters.hp)
+                        {
+                            var hp = (int) parameters.hp;
+                            var car = session.Query<Car>().FirstOrDefault(x => x.HorsePowers == hp);
+                            return Response.AsJson(car);
+                        }
+                        var cars = session.Query<Car>().ToList();
+                        return Response.AsJson(cars);
                     }
-                    var cars = linqCars.Where(x => x.HorsePowers <= 500).ToList();
-                    return Response.AsJson(cars);
                 };
             Get["/init/{count?100}"] = parameters =>
                 {
-                    var server = new CouchServer();
-                    var db = server.GetDatabase("trivial");
-                    for (var i = 0; i < parameters.count; i++)
+                    var database = new CouchDatabase("http://localhost:5984/");
+                    using (var session = database.OpenSession("trivial"))
                     {
-                        var car = new Car("Saab", (93 + i).ToString(), 170 + i);
-                        db.SaveDocument(car);
+                        for (var i = 0; i < parameters.count; i++)
+                        {
+                            var car = new Car("Saab", (93 + i).ToString(), 170 + i);
+                            session.Store(car);
+                        }
+                        session.SaveChanges();
                     }
                     return string.Format("Generated {0} cars.", parameters.count);
                 };
@@ -55,11 +58,11 @@ namespace NancyTest.Modules
         public DateTime CurrentDate { get; set; }
     }
 
-    public class Car : CouchDocument
+    public class Car
     {
-        public int HorsePowers;
-        public string Make;
-        public string Model;
+        public int HorsePowers { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
 
         public Car()
         {
@@ -74,31 +77,6 @@ namespace NancyTest.Modules
         }
 
         #region CouchDocument Members
-
-        public override void WriteJson(JsonWriter writer)
-        {
-            // This will write id and rev
-            base.WriteJson(writer);
-
-            writer.WritePropertyName("docType");
-            writer.WriteValue("car");
-            writer.WritePropertyName("Make");
-            writer.WriteValue(Make);
-            writer.WritePropertyName("Model");
-            writer.WriteValue(Model);
-            writer.WritePropertyName("Hps");
-            writer.WriteValue(HorsePowers);
-        }
-
-        public override void ReadJson(JObject obj)
-        {
-            // This will read id and rev
-            base.ReadJson(obj);
-
-            Make = obj["Make"].Value<string>();
-            Model = obj["Model"].Value<string>();
-            HorsePowers = obj["Hps"].Value<int>();
-        }
 
         #endregion
     }
